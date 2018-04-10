d2f13b95e5d168a43dc520ea9286a8fe73271323
Samuel <samuelchen@Samuels-MacBook-Air.local>
2018-04-09 23:09:42 -0400

    add a file
    add file

---
+++ src/main/java/Objects/User.java
@@ -0,0 +1,29 @@
+package Objects;
+
+public class User {
+	String firstName;
+	String lastName;
+	String email;
+	Byte[] password;
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setFirstName(String f) {
+		firstName = f;
+	}
+	public void setLastName(String l) {
+		lastName = l;
+	}
+	public void setEmail(String e) {
+		email = e;
+	}
+	
+	
+}
+
