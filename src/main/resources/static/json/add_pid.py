f460e2a09d5a5dc9f1d734ae871d6c62def8780f
Zhi Zou <zhi.zou@stonybrook.edu>
2018-05-08 23:22:59 -0400

    o

--- src/main/resources/static/json/add_pid.py
+++ src/main/resources/static/json/add_pid.py
@@ -1,15 +1,15 @@
 import json
 #
-with open('ID_final.json') as json_data:
+with open('KS_final.json') as json_data:
     d = json.load(json_data)
 count = 0
 
 for i in d["features"]:
-    print(i.keys())
+    #print(i.keys())
     if "pid" not in i:
         i["pid"] = count
         count += 1
     print(i["pid"])
 
-with open('ID_final.json','w') as outfile:
+with open('KS_final.json','w') as outfile:
     d = json.dump(d,outfile, indent = 1)
